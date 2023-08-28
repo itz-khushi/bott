bf5122b9711be2204f047b69262bcc84119ab566
Khushi <100374083+itz-khushi@users.noreply.github.com>
2023-08-28 17:57:04 +0530

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,27 @@
+function isToday(dateStringInUTC) {
+  _date = new Date(dateStringInUTC);
+  let day = _date.getDate();
+  let month = _date.getMonth();
+  let year = _date.getYear();
+
+  now = new Date();
+  let nowDay = now.getDate();
+  let nowMonth = now.getMonth();
+  let nowYear = now.getYear();
+
+  if (year == nowYear && month == nowMonth && day == nowDay) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// convert JSON to String, and format the text to print well.
+function jsonToString(value) {
+  return JSON.stringify(value, null, 4);
+}
+
+module.exports = {
+  jsonToString,
+  isToday,
+};
